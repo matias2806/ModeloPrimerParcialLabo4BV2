60c908139009dffdf51ba73b02718c23ef9befc4
matias2806 <matias.palmieri@gmail.com>
2021-05-05 20:29:12 -0300

    Se agrega listado de peliculas

--- src/app/components/listado-peliculas/listado-peliculas.component.ts
+++ src/app/components/listado-peliculas/listado-peliculas.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Pelicula } from 'src/app/clases/pelicula/pelicula';
+import { PeliculasService } from 'src/app/services/peliculas/peliculas.service';
+import { Actor } from '../../clases/actor/actor';
 @Component({
   selector: 'app-listado-peliculas',
   templateUrl: './listado-peliculas.component.html',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoPeliculasComponent implements OnInit {
 
-  constructor() { }
+  @Input() actorElejido: Actor;
+  listaPeliculas: Pelicula[];
+  peliculasEnLasQueParticipo: Pelicula[];
+
+  constructor(private _Pservice : PeliculasService) { 
+    this._Pservice.traerTodos().subscribe(peli => {
+
+      this.listaPeliculas = peli;
+      console.log(this.listaPeliculas);
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  enCualesParticipo():boolean{
+    var aux = false;
+    if(this.actorElejido){
+      this.peliculasEnLasQueParticipo = new Array<Pelicula>();
+      this.listaPeliculas.forEach(peli => {
+        peli.actores.forEach(actor => {
+          if(actor.nombre == this.actorElejido.nombre && actor.apellido == this.actorElejido.apellido){
+            this.peliculasEnLasQueParticipo.push(peli);
+            aux = true;
+          }
+        });
+      });
+    }
+    console.log(this.peliculasEnLasQueParticipo);
+    return aux;
   }
 
 }
